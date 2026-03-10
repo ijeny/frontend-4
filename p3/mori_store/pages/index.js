f7aee9c708fd51b24c22638d5e59a8cc6ea45045
ijeny <imtinan.jmb6628@gmail.com>
2026-03-10 10:42:04 +0700

    up

---
+++ p3/mori_store/pages/index.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import { useState } from "react";
+
+// DATA PRODUK
+const products = [
+  {
+    id: 1,
+    name: "Alana",
+    price: 250000,
+    image: "/images/alana.jpg",
+    sold: 110,
+  },
+  {
+    id: 1,
+    name: "Alana",
+    price: 250000,
+    image: "/images/alana.jpg",
+    sold: 110,
+  },
+];
